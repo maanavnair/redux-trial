7e125032931e20e40b37cea2cbd34d7bb55ebc66
Maanav Nair <maanavnair20042@gmail.com>
2024-09-06 19:21:44 +0530

    implemented the useSelector

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
 
 const Todos = () => {
+
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch();
+
   return (
-    <div>Todos</div>
+    <>
+      <div>Todos</div>
+      {todos.map((todo) => (
+        <li key={todos.id}>
+          {todo.text}
+          <button onClick={() => dispatch(removeTodo(todo.id))}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </>
   )
 }
 
