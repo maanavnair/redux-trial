34503ac48accb8adf891dd649557d559a8d9006c
Maanav Nair <maanavnair20042@gmail.com>
2024-09-06 10:28:38 +0530

    finished store

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from '../features/todo'
 
-export const store = configureStore({});
+export const store = configureStore({
+    reducer: todoReducer
+});
